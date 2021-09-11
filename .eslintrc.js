8dce4e37e90755d90a52b91d91804c8c7cedcc00
alexweininger <alex.weininger@live.com>
2021-09-10 23:29:32 -0700

    Turn rule off

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
-    "eslintConfig": {
-        "extends": ["react-app", "shared-config"],
-        "overrides": [
-            {
-                "files": ["**/*.ts?(x)"],
-                "rules": {
-                    "@typescript-eslint/no-unused-vars": "off"
-                }
+    "extends": ["react-app"],
+    "overrides": [
+        {
+            "files": ["**/*.ts?(x)"],
+            "rules": {
+                "@typescript-eslint/no-unused-vars": "off",
+                "react-hooks/exhaustive-deps": "off"
             }
-        ]
-    }
+        }
+    ]
 }
